beb128fb4a027dc76a9b0addd7a6c8037f43ef40
Balazs Bucsay <balazs.bucsay@mantrainfosec.com>
2024-12-16 20:56:43 +0000

    comment added

--- solutions/01caesar/01caesar-unicorn-hook.py
+++ solutions/01caesar/01caesar-unicorn-hook.py
@@ -50,7 +50,7 @@ uc.mem_write(ADDRESS, binary)
 # Set the string and offset values in memory
 input_str = b"Hello Unicorn!"
 offset = 3
-length = 14
+length = 14 # strlen(input_str)
 uc.mem_write(ADDRESS + (17 * 1024), input_str)  # Address where input_str is stored (binary 16K, string at 17K)
 uc.reg_write(UC_X86_REG_RDI, ADDRESS + (17 * 1024))  # Set the first argument (address of the string)
 uc.reg_write(UC_X86_REG_RSI, offset)  # Set the second argument (offset)
