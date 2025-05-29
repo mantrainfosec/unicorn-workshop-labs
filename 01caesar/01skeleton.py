810a2e1582a8552a640b22a0ffb84b5e606695ed
Balazs Bucsay <balazs.bucsay@mantrainfosec.com>
2025-05-29 09:57:11 +0100

    01 stack&heap changes

--- 01caesar/01skeleton.py
+++ 01caesar/01skeleton.py
@@ -18,22 +18,26 @@ with open("[HERE]", "rb") as f:
     binary = f.read()
 
 # Set the memory address where the code will be loaded (base address)
-ADDRESS = [HERE]
+BINARY = [HERE]
+STACK = [HERE]
+HEAP = [HERE]
 
 # Create an instance of the Unicorn Engine emulating x64
 uc = Uc(UC_ARCH_X86, UC_MODE_64)
 
 # Map memory for the code
-uc.mem_map(ADDRESS, [HERE])  # 2 MB
+uc.mem_map(BINARY, [HERE])  # 2 MB
+uc.mem_map(STACK, [HERE])  # 1 MB
+uc.mem_map(HEAP, [HERE])  # 1 MB
 
 # Write the ELF binary to the memory
-uc.mem_write(ADDRESS, binary)
+uc.mem_write(BINARY, binary)
 
 # Set the string and offset values in memory
 input_str = b"Hello Unicorn!"
 offset = 3
 length = len(input_str)
-uc.mem_write(ADDRESS + [HERE], input_str)  # Address where input_str is stored (binary 16K, string at 17K)
+uc.mem_write(HEAP, input_str)  # Address where input_str is stored (binary 16K, string at 17K)
 uc.reg_write(UC_X86_REG_[HERE], [HERE])  # Set the first argument (address of the string)
 uc.reg_write(UC_X86_REG_[HERE], [HERE])  # Set the second argument (offset)
 uc.reg_write(UC_X86_REG_[HERE], [HERE])  # Set the third argument (length)
@@ -42,10 +46,10 @@ uc.reg_write(UC_X86_REG_[HERE], [HERE])  # Set the stack (RSP)
 
 # Emulate code execution
 try:
-    uc.emu_start(ADDRESS + [HERE], ADDRESS + [HERE])  # Address of the caesar_cipher call
+    uc.emu_start(BINARY + [HERE], BINARY + [HERE])  # Address of the caesar_cipher call
 
     # Read the result from memory (assuming the C program prints the result)
-    result = uc.mem_read(ADDRESS + [HERE], len(input_str)).decode("utf-8")
+    result = uc.mem_read(HEAP, len(input_str)).decode("utf-8")
     print(f"Encoded string: {result}")
 
 except UcError as e:
--- solutions/01caesar/01caesar-unicorn-hook.py
+++ solutions/01caesar/01caesar-unicorn-hook.py
@@ -36,38 +36,43 @@ with open("01caesar", "rb") as f:
     binary = f.read()
 
 # Set the memory address where the code will be loaded (base address)
-ADDRESS = 0x1000000
+BINARY = 0x100000
+STACK = 0x400000
+HEAP = 0x500000
 
 # Create an instance of the Unicorn Engine emulating x64
 uc = Uc(UC_ARCH_X86, UC_MODE_64)
 
 # Map memory for the code
-uc.mem_map(ADDRESS, 2 * 1024 * 1024)  # 2 MB
+uc.mem_map(BINARY, 2 * 1024 * 1024)  # 2 MB
+uc.mem_map(STACK, 1024 * 1024)  # 1 MB
+uc.mem_map(HEAP, 1024 * 1024)  # 1 MB
 
 # Write the ELF binary to the memory
-uc.mem_write(ADDRESS, binary)
+uc.mem_write(BINARY, binary)
 
 # Set the string and offset values in memory
 input_str = b"Hello Unicorn!"
 offset = 3
-length = 14 # strlen(input_str)
-uc.mem_write(ADDRESS + (17 * 1024), input_str)  # Address where input_str is stored (binary 16K, string at 17K)
-uc.reg_write(UC_X86_REG_RDI, ADDRESS + (17 * 1024))  # Set the first argument (address of the string)
+length = strlen(input_str)
+uc.mem_write(HEAP, input_str)  # Address where input_str is stored
+uc.reg_write(UC_X86_REG_RDI, HEAP)  # Set the first argument (address of the string)
 uc.reg_write(UC_X86_REG_RSI, offset)  # Set the second argument (offset)
 uc.reg_write(UC_X86_REG_RDX, length)  # Set the third argument (length)
 
-uc.reg_write(UC_X86_REG_RSP, ADDRESS + (18 * 1024))  # Set the second argument (offset)
-uc.hook_add(UC_HOOK_CODE, hook_code, None, ADDRESS + 0x1189, ADDRESS + 0x12AC)
+uc.reg_write(UC_X86_REG_RSP, STACK + 512)  # Set the second argument (offset)
+
+uc.hook_add(UC_HOOK_CODE, hook_code, None, BINARY + 0x1189, BINARY + 0x12AC)
 #uc.hook_add(UC_HOOK_BLOCK, hook_block)
 uc.hook_add(UC_HOOK_MEM_WRITE, hook_mem_access)
 uc.hook_add(UC_HOOK_MEM_READ, hook_mem_access)
 
 # Emulate code execution
 try:
-    uc.emu_start(ADDRESS + 0x1189, ADDRESS + 0x12AC)  # Address of the caesar_cipher call
+    uc.emu_start(BINARY + 0x1189, BINARY + 0x12AC)  # Address of the caesar_cipher call
 
     # Read the result from memory (assuming the C program prints the result)
-    result = uc.mem_read(ADDRESS + (17 * 1024), len(input_str)).decode("utf-8")
+    result = uc.mem_read(HEAP, len(input_str)).decode("utf-8")
     print(f"Encoded string: {result}")
 
 except UcError as e:
